83c93a61c0558a8dd3f7892c06b2326b4a417dae
neyvuHN <mysteriousvyvy@gmail.com>
2026-04-23 16:29:53 +0700

    Add project files.

---
+++ De1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace De1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
